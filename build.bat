501a121adea5b3396f7a0b3897648312d7809bea
Johann Gerell <johann@gerell.net>
2020-12-16 17:46:10 +0100

    Minor build.bat changes

--- build.bat
+++ build.bat
@@ -1,18 +1,18 @@
 @echo off
 
-if "%1"=="debug" (
-    set "flags=/Od"
-) else if "%1"=="release" (
-    set "flags=/O2"
-) else (
-    echo Syntax: %0 [debug^|release]
-    exit /b 1
-)
+if "%1"=="debug" set "optimization=/Od" && goto :build
+if "%1"=="release" set "optimization=/O2" && goto :build
+
+echo Syntax: %0 [debug^|release] && exit /b 1
+
+:build
 
 set "output_dir=%1"
+set "output_exe=%output_dir%\wincontext.exe"
+set "output=/Fe^"%output_exe%^" /Fo^"%output_dir%/^" /Fd^"%output_dir%/^""
+set "input=main.cpp win32userdata.cpp win32api.cpp"
+set "flags=/nologo %optimization% /W4 /WX /Zi"
+
 if not exist "%output_dir%" mkdir %output_dir%
 
-set "output_exe=%output_dir%\wincontext.exe"
-set "sources=main.cpp win32userdata.cpp win32api.cpp"
-
-cl.exe /nologo %flags% /W4 /WX /Zi /Fe"%output_exe%" /Fo"%output_dir%/" /Fd"%output_dir%/" %sources% /link /SUBSYSTEM:WINDOWS user32.lib
+cl.exe %flags% %output% %input% /link /SUBSYSTEM:WINDOWS user32.lib
