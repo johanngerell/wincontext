9006b401e492fd95c5524070d474951f765d7c27
Johann Gerell <johann@gerell.net>
2021-01-01 17:38:18 +0100

    Minor build.bat change

--- build.bat
+++ build.bat
@@ -1,18 +1,22 @@
 @echo off
 
-if "%1"=="debug" set "optimization=/Od" && goto :build
+if "%1"=="debug" set "optimization=/RTCscu /D_ALLOW_RTCc_IN_STL /Od" && goto :build
 if "%1"=="release" set "optimization=/O2" && goto :build
 
 echo Syntax: %0 [debug^|release] && exit /b 1
 
 :build
 
+set "input=main.cpp win32userdata.cpp win32api.cpp"
 set "output_dir=%1"
 set "output_exe=%output_dir%\wincontext.exe"
 set "output=/Fe^"%output_exe%^" /Fo^"%output_dir%/^" /Fd^"%output_dir%/^""
-set "input=main.cpp win32userdata.cpp win32api.cpp"
-set "flags=/nologo /EHsc %optimization% /W4 /WX /Zi /std:c++17"
+set "cflags=/nologo /EHsc %optimization% /W4 /WX /Zi /std:c++17 %output% %input%"
+set "libs=user32.lib"
+set "lflags=/SUBSYSTEM:WINDOWS /DEBUG /INCREMENTAL:NO /OPT:REF /OPT:ICF %libs%"
 
 if not exist "%output_dir%" mkdir %output_dir%
 
-cl.exe %flags% %output% %input% /link /SUBSYSTEM:WINDOWS user32.lib
+: echo cl.exe %cflags% /link %lflags%
+
+cl.exe %cflags% /link %lflags%
