aa47a1c6e976cc7128c80acd8fcda186cfc0d54b
Johann Gerell <johann@gerell.net>
2021-01-20 20:52:33 +0100

    Moved h/cpp files to src/

--- build.bat
+++ build.bat
@@ -11,21 +11,21 @@ echo Syntax: %0 [debug^|release^|test] && exit /b 1
 :build
 
 if "%1"=="test" (
-    set "input=test.cpp"
+    set "input=src\test.cpp"
     set "output_dir=test"
     set "output_exe=test.exe"
     set "lflags=/DEBUG /INCREMENTAL:NO /OPT:REF /OPT:ICF"
 ) else (
-    set "input=main.cpp win32_userdata.cpp win32_api.cpp"
+    set "input=src\main.cpp src\win32_userdata.cpp src\win32_api.cpp"
     set "output_dir=%1"
     set "output_exe=wincontext.exe"
     set "lflags=/SUBSYSTEM:WINDOWS /DEBUG /INCREMENTAL:NO /OPT:REF /OPT:ICF user32.lib"
 )
 
 set "inc_dir=/I submodules\jg\inc"
-set "output=/Fe^"%output_dir%\%output_exe%^" /Fo^"%output_dir%/^" /Fd^"%output_dir%/^""
+set "output=/Fe^"build\%output_dir%\%output_exe%^" /Fo^"build\%output_dir%/^" /Fd^"build\%output_dir%/^""
 set "cflags=/nologo /EHsc %optimization% /W4 /WX /Zi /std:c++17 %inc_dir% %output% %input%"
 
-if not exist "%output_dir%" mkdir %output_dir%
+if not exist "build\%output_dir%" mkdir build\%output_dir%
 
 cl.exe %cflags% /link %lflags%
